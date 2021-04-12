10ad718587287b73c7acc5f44b633e1aca887300
andylamax <andylamax@programmer.net>
2021-04-12 11:09:01 +0300

    Releasing version 0.0.31

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -1,14 +1,14 @@
 object vers {
-    val kotlin = "1.4.21"
+    val kotlin = "1.4.32"
     val nexus_staging = "0.22.0"
 
     object asoft {
-        val expect = "0.0.20"
-        val builders = "1.3.0"
-        val duality = "0.0.30"
+        val expect = "0.0.21"
+        val builders = "1.3.24"
+        val duality = "0.0.31"
     }
 
     object kotlinx {
-        val serialization = "1.0.1"
+        val serialization = "1.1.0"
     }
 }
--- test/result-test-expect/build.gradle.kts
+++ test/result-test-expect/build.gradle.kts
@@ -7,7 +7,26 @@ plugins {
 }
 
 kotlin {
-    multiplatformLib()
+    jvm { library() }
+    js(IR) { library() }
+    val darwinTargets = listOf(
+        watchosArm64(),
+        watchosArm32(),
+        watchosX64(),
+        watchosX86(),
+        macosX64(),
+        iosArm64(),
+        iosArm32(),
+        iosX64(),
+        tvosArm64(),
+        tvosX64()
+    )
+
+    val linuxTargets = listOf(
+        linuxArm32Hfp(),
+        linuxArm64(),
+        linuxX64(),
+    )
     sourceSets {
         val commonMain by getting {
             dependencies {
