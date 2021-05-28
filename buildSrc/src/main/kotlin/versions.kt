b730140f236b3bc7d7de6dfbc7f4e13124a27509
andylamax <andylamax@programmer.net>
2021-05-28 06:33:30 +0300

    Bumped kotlin from 1.5.0 to 1.5.10

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -1,12 +1,12 @@
 object vers {
-    val kotlin = "1.5.0"
+    val kotlin = "1.5.10"
     val nexus_staging = "0.22.0"
     val ktor = "1.5.0"
 
     object asoft {
         val expect = "0.0.30"
         val builders = "1.3.30"
-        val duality = "0.0.40"
+        val duality = "0.0.50"
     }
 
     object kotlinx {
